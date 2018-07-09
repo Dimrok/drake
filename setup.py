5be938f0b49e5f1bcd6ce0e0152af95d26e671b9
Antony MECHIN <antony.mechin@docker.com>
2018-07-09 16:11:26 +0200

    Add setup.py.
    Signed-off-by: Antony MECHIN <antony.mechin@docker.com>

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open('README.mdown', 'r') as fh:
+  long_description = fh.read()
+
+with open('requirements.txt', 'r') as fr:
+  dependencies = fr.read().strip().split('\n')
+
+setuptools.setup(
+  name = 'drake',
+  version = '0.2.0',
+  author = 'Quentin (mefyl) Hocquet',
+  author_email = 'mefyl@gruntech.org',
+  description = 'The well-formed build system',
+  long_description = long_description,
+  long_description_content_type = 'text/markdown',
+  url = 'https://github.com/Dimrok/drake',
+  package_dir = {'': 'src'},
+  packages = setuptools.find_packages('src'),
+  classifiers = (
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+    'Operating System :: OS Independent',
+  ),
+  install_requires = dependencies
+)

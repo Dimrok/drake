72873a9b50af4094cf1328f3937dc61b7c29bece
Antony MECHIN <antony.mechin@docker.com>
2018-08-08 09:45:09 +0200

    setup.py: Bump version from 0.2.1 to 0.2.2.
    Signed-off-by: Antony MECHIN <antony.mechin@docker.com>

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('requirements.txt', 'r') as fr:
 
 setuptools.setup(
   name = 'drake',
-  version = '0.2.1',
+  version = '0.2.2',
   author = 'Quentin (mefyl) Hocquet',
   author_email = 'mefyl@gruntech.org',
   description = 'The well-formed build system',
